cfee861a39f8cb05ab4c5a95992b02bf1d30839c
ben-lu-uw <b54lu@uwaterloo.ca>
2021-04-16 17:56:32 -0400

    Update main process

--- src/com/bl/isp/isp.java
+++ src/com/bl/isp/isp.java
@@ -1,19 +1,35 @@
 package com.bl.isp;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 public class isp {
-    public static void main(String[] args) {
-        String inputPath = "ENTER IMAGE PATH";
-        String bOutputPath = "ENTER MID OUTPUT PATH";
-        String oOutputPath = "ENTER FINAL OUTPUT PATH";
+    public static void main(String[] args) throws IOException {
+        String inputPath = "FULL INPUT PATH";
+        String outputPath = "FULL OUTPUT PATH";
 
-        GaussianBlur blur = new GaussianBlur();
-        try {
-            blur.applyBlur(3, inputPath, bOutputPath);
-            Outline.combineOutline(bOutputPath, oOutputPath);
-        } catch (IOException e) {
-            e.printStackTrace();
+        RowMatrix<Pixel> rgbMatrix = ReadImage.rgbMatrix(inputPath);
+        RowMatrix<Pixel> blurredMatrix = GaussianBlur.apply5x5Filter(rgbMatrix);
+        RowMatrix<Gradient> gradientMatrix = Edge.generate(blurredMatrix);
+        RowMatrix<Gradient> suppressedMatrix = Edge.suppress(gradientMatrix);
+        RowMatrix<Gradient> finalMatrix = Edge.trackConnection(suppressedMatrix);
+
+        int height = finalMatrix.getRows().size();
+        int width = finalMatrix.getRows().get(0).getCells().size();
+
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+
+        for (int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                int g = finalMatrix.getRows().get(y).getCells().get(x).getG();
+                int bit = (g << 16) | (g << 8) | g;
+
+                bufferedImage.setRGB(x, y, bit);
+            }
         }
+
+        ImageIO.write(bufferedImage, "png", new File(outputPath));
     }
 }
